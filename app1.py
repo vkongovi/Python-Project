d2232df2c031b64ea224ad549155661be0d83791
vkongovi <39563920+vkongovi@users.noreply.github.com>
2019-11-17 08:42:09 -0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,21 @@
+print("Vinaya Kongovi")
+print('o-----')
+print('||||')
+print('*' * 10)
+price =50
+price=60
+rating=6.9
+name='vinaya'
+is_published=True
+print(price)
+print(price*rating)
+patient_name='John Smith'
+patient_age=20
+new_patient='False'
+if new_patient :
+    print(patient_name+ ' is a new patient of age '+str(patient_age))
+else:
+    print(patient_name+' is an existing patient of age '+str(patient_age))
+
+user_name=input('What is your name?')
+print('Hi !'+user_name)
